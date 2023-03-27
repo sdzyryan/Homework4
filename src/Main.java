a63869c9f21f2ff2700276844a6b09e82c67f8e8
sdzyryan <sdzyryan@gmail.com>
2023-03-27 20:34:42 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-        int age = 20;
+        int age = 15;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
         } else {
-            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать.");
         }
     }
 
