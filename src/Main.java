8991bac58510661d9b9e73d76709b30a8551ff29
sdzyryan <sdzyryan@gmail.com>
2023-03-27 20:32:27 +0500

    task1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+                 }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+    }
+}
+
